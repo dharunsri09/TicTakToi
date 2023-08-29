1a0c2fce5e83505cd3d002779f81841fdc830a76
DHARUN M <115633964+dharunsri09@users.noreply.github.com>
2023-08-29 22:45:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ function addGo(e){
 function wins(){
     const alls=document.querySelectorAll(".square")
     const winningCombo=[
-        [0,1,2],[0,4,8],[2,3,6],
-        [3,4,5],[6,7,8],[0,3,6],
+        [0,1,2],[0,4,8],[0,3,6],
+        [3,4,5],[6,7,8],[1,4,7],
         [2,4,6],[2,5,8]
     ]
     winningCombo.forEach(array=>{
